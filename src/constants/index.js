16a34c8625b6af2f2f6937bb5530a3d2233616c2
Nattapon Nimakul <totizy@gmail.com>
2020-01-16 19:59:59 +0700

    price in dollar

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,81 +2,81 @@ export const menuList = {
   xbox: [{
     title: 'Xbox 1 / 1 TB',
     subtitle: 'C889842353600',
-    price: 20290,
+    price: 679,
     image: 'menu/xbox/xbox-1.jpg'
   },
   {
     title: 'Xbox 1 / 1 TB',
     subtitle: 'C889842438208',
-    price: 10990,
+    price: 369,
     image: 'menu/xbox/xbox-2.jpg'
   },
   {
     title: 'Xbox 1 / 1 TB',
     subtitle: 'C889842353631',
-    price: 18990,
+    price: 639,
     image: 'menu/xbox/xbox-3.jpg'
   },
   {
     title: 'Xbox 1 / 1 TB',
     subtitle: 'C889842408973',
-    price: 18990,
+    price: 639,
     image: 'menu/xbox/xbox-4.jpg'
   },
   {
     title: 'XBox360 Blue',
     subtitle: 'C885370862744',
-    price: 13790,
+    price: 459,
     image: 'menu/xbox/xbox-360-blue.jpg'
   }],
 
   nintendo: [{
     title: 'Pokemon Shield',
     subtitle: 'C045496596620',
-    price: 1890,
+    price: 69,
     image: 'menu/nintendo/pokemon-shield.jpg'
   },
   {
     title: 'Pokemon Sword',
     subtitle: 'C045496596583',
-    price: 1890,
+    price: 69,
     image: 'menu/nintendo/pokemon-sword.jpg'
   },
   {
     title: 'JOY-CON',
     subtitle: 'C4902370536751',
-    price: 1490,
+    price: 49,
     image: 'menu/nintendo/joy-con.jpg'
   },
   {
     title: 'Pro Controller',
     subtitle: 'C045496430528',
-    price: 2390,
+    price: 79,
     image: 'menu/nintendo/pro-controller.jpg'
   }],
 
   playstation: [{
     title: 'Dualshock4 wiless',
     subtitle: 'C711719532958',
-    price: 2300,
+    price: 79,
     image: 'menu/playstation/dualshock4-wiless.jpg'
   },
   {
     title: 'PS4 FIFA20 Bundle',
     subtitle: 'C889842438208',
-    price: 10990,
+    price: 369,
     image: 'menu/playstation/ps4-fifa20-bundle.jpg'
   },
   {
     title: 'PS1 Classic (Asia)',
     subtitle: 'C4548736094345',
-    price: 2490,
+    price: 89,
     image: 'menu/playstation/ps1-classic-asia.jpg'
   },
   {
     title: 'PS4 Pro 1TB (Jap)',
     subtitle: 'C4948872414241',
-    price: 16690,
+    price: 559,
     image: 'menu/playstation/ps4-pro-1tb-jap.jpg'
   }]
 }
